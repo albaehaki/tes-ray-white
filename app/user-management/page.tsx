036e7c646c2405f0061d3a8736b6f3288d9892f5
albaehaki <zackyalbaehaki@gmail.com>
2023-08-17 16:45:21 +0700

    menambahkan table untuk layar smartphone

--- app/user-management/page.tsx
+++ app/user-management/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React, {useState} from "react";
+import React, { useState } from "react";
 import {
   MagnifyingGlassCircleIcon,
   MagnifyingGlassIcon,
@@ -7,14 +7,19 @@ import {
   FunnelIcon,
   ChevronRightIcon,
   ChevronLeftIcon,
+  ChevronDownIcon,
 } from "@heroicons/react/24/outline";
 // components
-import  FormPopUp from "@/components/formPopUp"
-
+import FormPopUp from "@/components/formPopUp";
 
 const CustomerTable: React.FC = () => {
   // Dummy data for customer table
   const [popupOpen, setPopupOpen] = useState(false);
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleDropdown = () => {
+    setIsOpen(!isOpen);
+  };
 
   const openPopup = () => {
     setPopupOpen(true);
@@ -24,7 +29,6 @@ const CustomerTable: React.FC = () => {
     setPopupOpen(false);
   };
 
-
   const customers = [
     { id: 1, name: "John Doe", phone: "1234567890", email: "john@example.com" },
     { id: 2, name: "John Doe", phone: "1234567890", email: "john@example.com" },
@@ -33,7 +37,7 @@ const CustomerTable: React.FC = () => {
 
   return (
     <div className="flex flex-col items-center">
-      <div className="w-full max-w-screen-lg mt-8">
+      <div className="w-full lg:max-w-screen-lg md:max-w-screen-md sm:max-w-screen-sm px-5 mt-8">
         <div className="flex items-center mb-4 relative bg-gray-100 rounded-md p-2">
           <input
             type="text"
@@ -45,7 +49,7 @@ const CustomerTable: React.FC = () => {
           </div>
           <button className="flex p-2 ml-2 border border-slate-800 bg-white rounded-md shadow-sm">
             <FunnelIcon className="w-5 h-5 mr-2" />
-            Filter
+            <p className="hidden md:block">Filter</p>
           </button>
         </div>
         <div className="overflow-x-auto">
@@ -53,11 +57,11 @@ const CustomerTable: React.FC = () => {
             <button className="text-gray-800 py-2 px-1 hover:border-b-2 border-gray-800 text-sm">
               Menu 1
             </button>
-            
+
             {/* Tambahkan lebih banyak menu sesuai kebutuhan */}
           </div>
         </div>
-        <div className="">
+        <div className=" hidden sm:block">
           <table className="min-w-full border ">
             <thead className="bg-slate-800 text-white text-left">
               <tr>
@@ -69,14 +73,21 @@ const CustomerTable: React.FC = () => {
               </tr>
             </thead>
             {customers.map((customer) => (
-              <tbody className={`${customer.id % 2 !== 0 ? `bg-gray-100`: ``}`}>
+              <tbody
+                className={`${customer.id % 2 !== 0 ? `bg-gray-100` : ``}`}
+              >
                 <tr key={customer.id}>
                   <td className="px-2 py-3">{customer.id}</td>
                   <td className="px-2 py-3">{customer.name}</td>
                   <td className="px-2 py-3">{customer.phone}</td>
                   <td className="px-2 py-3">{customer.email}</td>
                   <td className="px-2 py-3">
-                    <button onClick={() => {openPopup()}} className="mr-2 text-gray-800 border-collapse border-2 border-gray-800 px-3 py-1">
+                    <button
+                      onClick={() => {
+                        openPopup();
+                      }}
+                      className="mr-2 text-gray-800 border-collapse border-2 border-gray-800 px-3 py-1"
+                    >
                       Edit
                     </button>
                     <button className="text-white px-3 py-1 bg-red-500 ">
@@ -87,8 +98,32 @@ const CustomerTable: React.FC = () => {
               </tbody>
             ))}
           </table>
+          
         </div>
-        
+        {/* table smartphone view */}
+
+        <div className="relative w-full min-w-full block md:hidden">
+            <button
+              onClick={toggleDropdown}
+              className={`flex items-center bg-gray-100 space-x-2 relative py-3 px-6 w-full ${
+                isOpen ? `rounded-t-md` : `rounded-md`
+              }`}
+            >
+              <span className="text-sm ">Your Name</span>
+              <ChevronDownIcon className="w-6 h-6 justify-self-end absolute right-3" />
+            </button>
+            {isOpen && (
+              <div className="relative right-0 mt-0 bg-white border rounded-b-md border-gray-300 w-full">
+                <table className="table-auto w-full mx-3 my-2">
+                  <tr>
+                    <th className="text-left">Name </th>
+                    <td>:</td>
+                    <td>zacky al baehaki</td>
+                  </tr>
+                </table>
+              </div>
+            )}
+          </div>
       </div>
       {/* popup */}
       <FormPopUp isOpen={popupOpen} onClose={closePopup} />
@@ -110,26 +145,28 @@ const App: React.FC = () => {
         <CustomerTable />
       </div>
       <div className="flex justify-between items-center mt-4 bg-white px-10 py-3 rounded">
-          <div className="flex items-center">
-            <span className="mr-2">1 - 10</span>
-          </div>
-          <div className="flex items-center">
-            <span className="mr-2">This page you are on: {`  `}</span>
-            <select className="p-1 border-2 rounded-md shadow-sm border-gray-800">
-              <option>1</option>
-              <option>2</option>
-              {/* ... more page options */}
-            </select>
-            <div className="flex border-l-2 border-gray-800 ml-3">
+        <div className="flex items-center">
+          <span className="mr-2">1 - 10</span>
+        </div>
+        <div className="flex items-center">
+          <span className="mr-2 hidden md:block">
+            This page you are on: {`  `}
+          </span>
+          <select className="p-1 border-2 rounded-md shadow-sm border-gray-800">
+            <option>1</option>
+            <option>2</option>
+            {/* ... more page options */}
+          </select>
+          <div className="flex border-l-2 border-gray-800 ml-3">
             <button className="mr-2 ml-3 border-2 rounded-md shadow-sm border-gray-300 text-gray-300">
-                <ChevronLeftIcon className="w-6 h-6" />
+              <ChevronLeftIcon className="w-6 h-6" />
             </button>
             <button className="border-2 rounded-md shadow-sm border-gray-800">
-            <ChevronRightIcon className="w-6 h-6 " />
+              <ChevronRightIcon className="w-6 h-6 " />
             </button>
-            </div>
           </div>
         </div>
+      </div>
     </>
   );
 };
--- components/formPopUp.tsx
+++ components/formPopUp.tsx
@@ -32,8 +32,9 @@ const Popup = ({ isOpen, onClose }: any) => {
   return (
     <div className="fixed inset-0 flex justify-center items-center bg-black bg-opacity-50">
       <div className="bg-white p-6 rounded-lg shadow-md w-96">
-        <h2 className="text-xl font-bold mb-4">Form Data</h2>
+        <h2 className="text-xl text-center font-bold mb-4">Form Data</h2>
         <form onSubmit={handleSubmit}>
+            {/* name */}
           <div className="mb-3">
             <label htmlFor="name" className="block font-medium">
               Nama
@@ -47,8 +48,78 @@ const Popup = ({ isOpen, onClose }: any) => {
               className="border rounded-md px-2 py-1 w-full"
             />
           </div>
+            {/* category */}
+          <div className="mb-3">
+            <label htmlFor="name" className="block font-medium">
+            category
+            </label>
+            <select className="border rounded-md px-2 py-1 w-full">
+              <option>1</option>
+              <option>2</option>
+              {/* ... more page options */}
+            </select>
+          </div>
+            {/* office */}
+          <div className="mb-3">
+            <label htmlFor="name" className="block font-medium">
+            office
+            </label>
+            <select className="border rounded-md px-2 py-1 w-full">
+              <option>1</option>
+              <option>2</option>
+              {/* ... more page options */}
+            </select>
+          </div>
+            {/* phone */}
+          <div className="mb-3">
+            <label htmlFor="name" className="block font-medium">
+            phone
+            </label>
+            <input
+              type="number"
+              id="name"
+              name="name"
+              value={formData.name}
+              onChange={handleChange}
+              className="border rounded-md px-2 py-1 w-full"
+            />
+          </div>
+            {/* email */}
+          <div className="mb-3">
+            <label htmlFor="name" className="block font-medium">
+            email
+            </label>
+            <input
+              type="email"
+              id="name"
+              name="name"
+              value={formData.name}
+              onChange={handleChange}
+              className="border rounded-md px-2 py-1 w-full"
+            />
+          </div>
+            {/* status */}
+          <div className="mb-3">
+            <label htmlFor="name" className="block font-medium">
+            status
+            </label>
+            {/* <input
+              type="select"
+              id="name"
+              name="name"
+              value={formData.name}
+              onChange={handleChange}
+              className="border rounded-md px-2 py-1 w-full"
+            /> */}
+            <select className="border rounded-md px-2 py-1 w-full">
+              <option>1</option>
+              <option>2</option>
+              {/* ... more page options */}
+            </select>
+          </div>
+          
           {/* Tambahkan input lain sesuai kebutuhan */}
-          <div className="flex justify-end mt-4">
+          <div className="flex justify-evenly mt-4">
             <button
               type="button"
               onClick={onClose}
